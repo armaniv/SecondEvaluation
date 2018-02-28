c558b0b101f1010be6afc3b904319c9865a1ff90
armaniv <valearmani95@gmail.com>
2018-02-28 14:39:41 +0100

    Add test waxman

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ warnings.filterwarnings("ignore")
 graph_kind = set(["erdos", "waxman", "caveman"])
 
 
-def GeneraGrafoRandom(kind, seed=None):
+def GeneraGrafoRandom(kind, seed=None, dim=60, beta=0.09):
     if kind not in graph_kind:
         print "Unknown graph type"
         exit()
     if kind == "caveman":
         g = nx.relaxed_caveman_graph(8, 7, 0.12, seed=seed)
     if kind == "waxman":
-        g = nx.waxman_graph(200, alpha=0.9)
+        g = nx.waxman_graph(dim, alpha=1.2, beta=beta)
     if kind == "erdos":
         g = nx.gnp_random_graph(60, 0.08, seed=seed)
 
@@ -63,12 +63,10 @@ def GeneraGrafoRandom(kind, seed=None):
 
 def SommaPerditePerNodi(b, cut, interf, inter_cut):
 
+    candidates = []
     candidates = b.select_node_to_fail()
 
-    lossHPop = 0
-    lossHNonPop = 0
-    lossLSAPop = 0
-    lossLSANonPop = 0
+    lossHPop = lossHNonPop = lossLSAPop = lossLSANonPop = L_HPop = L_LSAPop = 0.0
     for cc in candidates:
         h, tc = b.compute_loss(cc[0], pop=True)
         hnp, tcnp = b.compute_loss(cc[0], pop=False)
@@ -77,13 +75,14 @@ def SommaPerditePerNodi(b, cut, interf, inter_cut):
         lossLSAPop += tc
         lossLSANonPop += tcnp
 
+    L_HPop = 1.0 - (lossHNonPop / lossHPop)
+    L_LSAPop = 1.0 - (lossLSANonPop / lossLSAPop)
     print "Pop-Ruting:{ L_h= ", 1.0 - (lossHNonPop / lossHPop), ", L_LSA= ", \
         1.0 - (lossLSANonPop / lossLSAPop), "}"
 
-    lossHCut = 0
-    lossHNonCut = 0
-    lossLSACut = 0
-    lossLSANonCut = 0
+#------------------------------------------------------------------------------
+
+    lossHCut = lossHNonCut = lossLSACut = lossLSANonCut = L_HCut = L_LSACut = 0.0
     for cc2 in candidates:
         h, tc = cut.compute_loss(cc2[0], pop=True)
         hnp, tcnp = cut.compute_loss(cc2[0], pop=False)
@@ -92,13 +91,14 @@ def SommaPerditePerNodi(b, cut, interf, inter_cut):
         lossLSACut += tc
         lossLSANonCut += tcnp
 
+    L_HCut = 1.0 - (lossHNonCut / lossHCut)
+    L_LSACut = 1.0 - (lossLSANonCut / lossLSACut)
     print "Pop-R  cut:{ L_h= ", 1.0 - (lossHNonCut / lossHCut), ", L_LSA= ", \
         1.0 - (lossLSANonCut / lossLSACut), "}"
 
-    lossHInt = 0
-    lossHNonInt = 0
-    lossLSAInt = 0
-    lossLSANonInt = 0
+#------------------------------------------------------------------------------
+
+    lossHInt = lossHNonInt = lossLSAInt = lossLSANonInt = L_HInt = L_LSAInt = 0
     for cc3 in candidates:
         h, hnp, tc, tcnp = interf.compute_loss_interfacce(cc3[0])
         lossHInt += h
@@ -106,13 +106,14 @@ def SommaPerditePerNodi(b, cut, interf, inter_cut):
         lossLSAInt += tc
         lossLSANonInt += tcnp
 
+    L_HInt = 1.0 - (lossHNonInt / lossHInt)
+    L_LSAInt = 1.0 - (lossLSANonInt / lossLSAInt)
     print "Pop-R inter:{ L_h= ", 1.0 - (lossHNonInt / lossHInt), ", L_LSA= ", \
         1.0 - (lossLSANonInt / lossLSAInt), "}"
 
-    lossHIntCut = 0
-    lossHNonIntCut = 0
-    lossLSAIntCut = 0
-    lossLSANonIntCut = 0
+#------------------------------------------------------------------------------
+
+    lossHIntCut = lossHNonIntCut = lossLSAIntCut = lossLSANonIntCut = L_HIntCut = L_LSAIntCut = 0
     for cc4 in candidates:
         h, hnp, tc, tcnp = inter_cut.compute_loss_interfacce(cc4[0])
         lossHIntCut += h
@@ -120,8 +121,14 @@ def SommaPerditePerNodi(b, cut, interf, inter_cut):
         lossLSAIntCut += tc
         lossLSANonIntCut += tcnp
 
+    L_HIntCut = 1.0 - (lossHNonIntCut / lossHIntCut)
+    L_LSAIntCut = 1.0 - (lossLSANonIntCut / lossLSAIntCut)
     print "Pop-R int+cut:{ L_h= ", 1.0 - (lossHNonIntCut / lossHIntCut),\
         ", L_LSA= ", 1.0 - (lossLSANonIntCut / lossLSAIntCut), "}"
+
+#------------------------------------------------------------------------------
+
+    return L_HPop, L_LSAPop, L_HCut, L_LSACut, L_HInt, L_LSAInt, L_HIntCut, L_LSAIntCut
 
 
 p = argparse.ArgumentParser()
@@ -142,18 +149,40 @@ if not args.type and not args.graph:
 
 if args.evluatioTest:
     if args.type:
-        for i in range(3):
-            graph = GeneraGrafoRandom(kind=args.type, seed=args.seed)
-            nx.write_graphml(graph, 'temp.graphml')
-            b = ComputeTheoreticalValues(graph_file='temp.graphml', cH=2, cTC=5)
-            cut = ComputeTheoreticalValues(graph_file='temp.graphml', cH=2, cTC=5, cent="Cut-point")
-            interf = ComputeTheoreticalValues(
-                graph_file='temp.graphml', cH=2, cTC=5, cent="Interfacce")
-            inter_cut = ComputeTheoreticalValues(
-                graph_file='temp.graphml', cH=2, cTC=5, cent="Inter-cut")
+        fil = open('waxman.dat', 'w')
+        if args.type == "waxman":
+            dizWaxman = {60: 0.089, 100: 0.077, 150: 0.061, 200: 0.052}
+            for diter in sorted(dizWaxman.iterkeys()):
+                L_HPop, L_LSAPop, L_HCut, L_LSACut, L_HInt, L_LSAInt, L_HIntCut, L_LSAIntCut = (
+                    [] for i in range(8))
 
-            SommaPerditePerNodi(b, cut, interf, inter_cut)
-            print "------------------------------------------------------------"
+                for i in range(2):
+                    print diter
+                    graph = GeneraGrafoRandom(kind=args.type, seed=args.seed,
+                                              dim=diter, beta=dizWaxman[diter])
+                    b = ComputeTheoreticalValues(graph=graph, cH=2, cTC=5)
+                    cut = ComputeTheoreticalValues(graph=graph, cH=2, cTC=5, cent="Cut-point")
+                    interf = ComputeTheoreticalValues(graph=graph, cH=2, cTC=5, cent="Interfacce")
+                    inter_cut = ComputeTheoreticalValues(graph=graph, cH=2, cTC=5, cent="Inter-cut")
+
+                    LHPop, LLSAPop, LHCut, LLSACut, LHInt, LLSAInt, LHIntCut, LLSAIntCut = SommaPerditePerNodi(
+                        b, cut, interf, inter_cut)
+
+                    L_HPop.append(LHPop)
+                    L_LSAPop.append(LLSAPop)
+                    L_HCut.append(LHCut)
+                    L_LSACut.append(LLSACut)
+                    L_HInt.append(LHInt)
+                    L_LSAInt.append(LLSAInt)
+                    L_HIntCut.append(LHIntCut)
+                    L_LSAIntCut.append(LLSAIntCut)
+                    print "------------------------------------------------------------"
+
+                nci = 2
+                print >> fil, diter, sum(L_HPop) / nci, sum(L_LSAPop) / nci, sum(L_HCut) / nci,\
+                    sum(L_LSACut) / nci, sum(L_HInt) / nci, sum(L_LSAInt) / nci,\
+                    sum(L_HIntCut) / nci, sum(L_LSAIntCut) / nci
+        fil.close()
         exit()
     else:
         exit()
@@ -163,15 +192,19 @@ if args.type:
     nx.write_graphml(graph, 'temp.graphml')  # necessario per permettere ripetibilita' dei test
     b = ComputeTheoreticalValues(graph_file='temp.graphml', cH=2, cTC=5)
     cut = ComputeTheoreticalValues(graph_file='temp.graphml', cH=2, cTC=5, cent="Cut-point")
-    interf = ComputeTheoreticalValues(graph_file='temp.graphml', cH=2, cTC=5, cent="Interfacce")
-    inter_cut = ComputeTheoreticalValues(graph_file='temp.graphml', cH=2, cTC=5, cent="Inter-cut")
+    interf = ComputeTheoreticalValues(graph_file='temp.graphml',
+                                      cH=2, cTC=5, cent="Interfacce")
+    inter_cut = ComputeTheoreticalValues(
+        graph_file='temp.graphml', cH=2, cTC=5, cent="Inter-cut")
 
 else:
     if args.graph:
         b = ComputeTheoreticalValues(graph_file=args.graph, cH=2, cTC=5)
         cut = ComputeTheoreticalValues(graph_file=args.graph, cH=2, cTC=5, cent="Cut-point")
-        interf = ComputeTheoreticalValues(graph_file=args.graph, cH=2, cTC=5, cent="Interfacce")
-        inter_cut = ComputeTheoreticalValues(graph_file=args.graph, cH=2, cTC=5, cent="Inter-cut")
+        interf = ComputeTheoreticalValues(graph_file=args.graph,
+                                          cH=2, cTC=5, cent="Interfacce")
+        inter_cut = ComputeTheoreticalValues(
+            graph_file=args.graph, cH=2, cTC=5, cent="Inter-cut")
 
 SommaPerditePerNodi(b, cut, interf, inter_cut)
 
